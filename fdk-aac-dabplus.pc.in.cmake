607537763f0320aac5e909208529e6c47168c4f4
Martin Hauke <mardnh@gmx.de>
2015-09-13 00:02:15 +0200

    cmake: rename lib to fdk-aac-dabplus

--- fdk-aac-dabplus.pc.in.cmake
+++ fdk-aac-dabplus.pc.in.cmake
@@ -1,12 +1,12 @@
-prefix=@LIBFDK-AAC_PC_PREFIX@
-exec_prefix=@LIBFDK-AAC_PC_EXEC_PREFIX@
-libdir=@LIBFDK-AAC_PC_LIBDIR@
-includedir=@LIBFDK-AAC_PC_INCLUDEDIR@
+prefix=@LIBFDK-AAC-DABPLUS_PC_PREFIX@
+exec_prefix=@LIBFDK-AAC-DABPLUS_PC_EXEC_PREFIX@
+libdir=@LIBFDK-AAC-DABPLUS_PC_LIBDIR@
+includedir=@LIBFDK-AAC-DABPLUS_PC_INCLUDEDIR@
 
 Name: Fraunhofer FDK AAC Codec Library with DAB+ Encoder
 Description: AAC codec library with DAB+ Encoder
-Version: @LIBFDK-AAC_PC_VERSION@
+Version: @LIBFDK-AAC-DABPLUS_PC_VERSION@
 URL: http://opendigitalradio.org
-Cflags: -I${includedir}/fdk-aac @LIBFDK-AAC_PC_CFLAGS@
-Libs: -L${libdir}/ @LIBFDK-AAC_PC_LIBS@
-Libs.private: @LIBFDK-AAC_PC_PRIV_LIBS@
+Cflags: -I${includedir}/fdk-aac @LIBFDK-AAC-DABPLUS_PC_CFLAGS@
+Libs: -L${libdir}/ @LIBFDK-AAC-DABPLUS_PC_LIBS@
+Libs.private: @LIBFDK-AAC-DABPLUS_PC_PRIV_LIBS@
